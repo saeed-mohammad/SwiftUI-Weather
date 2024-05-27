e21da90ef96c54faa72eac13bf01bfea14fba7ae
Mohammad saeed <sk.saeed008@gmial.com>
2024-05-27 20:35:13 +0530

    Initial Commit

---
+++ SwiftUI-Weather/SwiftUI_WeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_WeatherApp.swift
+//  SwiftUI-Weather
+//
+//  Created by saeed shaikh on 5/27/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_WeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
